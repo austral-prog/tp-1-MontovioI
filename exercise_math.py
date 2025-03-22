3e007d19e75927dd0474d28d81b25e593e562159
MontovioI <ilmontovio@mail.austral.edu.ar>
2025-03-22 20:19:31 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,23 @@
 def math():
+    # números
     a = 57
     b = 7
+
+    # operaciones
+    suma= a+b
+    resta= a-b
+    mult=a*b
+    prom=(a+b)/2
+    entero=int(a/b)
+    resto=a%b
+    div=a/b
+
+    # impresión de las operaciones 
+    print(suma)
+    print(resta)
+    print(mult)
+    print(prom)
+    print(entero)
+    print(resto)
+    print(div)
+
